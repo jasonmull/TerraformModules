58c6439bc1db4c0730541a6f51bfe1d1c8872035
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2024-01-07 20:38:46 -0600

    Create variables.tf

---
+++ VmwareCloneWindowsTemplate/variables.tf
@@ -0,0 +1,55 @@
+# vCenter Credentials
+
+variable "vcenter_username" {
+    description = "The username Terraform will use to login to vCenter"
+    type = string
+    sensitive = true
+}
+
+variable "vcenter_password" {
+    description  = "The password Terraform will use to login to vCenter"
+    type = string
+    sensitive = true
+}
+
+variable "vcenter_server" {
+    description = "The FQDN of the vCenter Terraform will connect to"
+    type = string
+}
+
+# VM Provisioning Variables
+
+variable "vcenter_datacenter" {
+    description = "The datacenter where the VM will be provisioned"
+    type = string
+}
+
+variable "vcenter_cluster" {
+    description = "The cluster where the VM will be provisioned"
+    type = string
+}
+
+variable "vcenter_datastore" {
+    description = "The datastore where the VM will be provisioned"
+    type = string
+}
+
+variable "vcenter_folder" {
+    description = "The folder where the VM will be provisioned"
+    type = string
+}
+
+variable "vcenter_network" {
+    description = "The network the VM will be connected to"
+    type = string
+}
+
+variable "vcenter_template" {
+    description = "The template the VM will be cloned from"
+    type = string
+}
+
+variable "vm_name" {
+    description = "The name of the VM"
+    type = string
+}
